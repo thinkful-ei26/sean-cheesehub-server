9876d581d20fddc90efec7b228e9710701f865a7
seankrummel <sean.krummel@gmail.com>
2018-12-21 10:18:34 -0600

    created an API endpoint

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.use(
   })
 );
 
+app.get('/api/cheeses', (req, res, next) => {
+  res.json(['Bath Blue', 'Barkham Blue', 'Buxton Blue', 'Cheshire Blue', 'Devon Blue', 'Dorset Blue Vinney', 'Dovedale',
+    'Exmoor Blue', 'Harbourne Blue', 'Lanark Blue', 'Lymeswold', 'Oxford Blue', 'Shropshire Blue', 'Stichelton', 'Stilton',
+    'Blue Wensleydale', 'Yorkshire Blue']);
+});
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
